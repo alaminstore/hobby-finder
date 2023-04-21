53e43ea9d4ddbc3c839c8e0ba174317899e224e8
alaminstore <alamin.piash.ap23@gmail.com>
2023-04-21 14:54:13 +0600

    data search function adjustment

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,13 @@ import Header from "./Header";
 import PushPinIcon from "@mui/icons-material/PushPin";
 import MyLocationIcon from "@mui/icons-material/MyLocation";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 const Map = () => {
   const [location, setLocation] = useState({ latitude: null, longitude: null });
   const [hobbies, setHobbies] = useState([]);
@@ -76,8 +82,8 @@ const Map = () => {
         hobby?.name?.toLowerCase().includes(hobbyType?.toLowerCase()) &&
         // getDistanceFromLatLonInKm()
         getDistanceFromLatLonInKm(
-          25.168282,
-          55.250286,
+          searchLatLng?.lat,
+          searchLatLng?.lng,
           hobby.latitude,
           hobby.longitude
         ) <= distance
@@ -100,71 +106,101 @@ const Map = () => {
         reset={handleReset}
         newSearchLatLng={setSearchLatLng}
       />
-      {location.latitude && (
-        <div style={{ height: "50vh", width: "50vw" }} className="mt-5 px-3">
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key: "AIzaSyAsprnJybW_KqoOy7cbmd9qQWx-wSxuKwU&libraries",
-            }}
-            defaultCenter={{
-              lat: location?.latitude,
-              lng: location?.longitude,
-            }}
-            center={center}
-            defaultZoom={12}
-            yesIWantToUseGoogleMapApiInternals
-          >
-            {hobbies?.map((hobby, index) => (
-              <LocationOnIcon
-                key={index}
-                color={"secondary"}
-                lat={hobby?.latitude}
-                lng={hobby?.longitude}
-                onClick={() => {
-                  setSelectedData(hobby.id);
-                  console.log("selectedData", hobby.id);
-                }}
-              />
-            ))}
+      <div className="flex justify-center items-center mt-5">
+        {location.latitude && (
+          <div style={{ height: "50vh" }} className="px-3 w-[50%]">
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: "AIzaSyAsprnJybW_KqoOy7cbmd9qQWx-wSxuKwU&libraries",
+              }}
+              defaultCenter={{
+                lat: location?.latitude,
+                lng: location?.longitude,
+              }}
+              center={center}
+              defaultZoom={12}
+              yesIWantToUseGoogleMapApiInternals
+            >
+              {hobbies?.map((hobby, index) => (
+                <LocationOnIcon
+                  key={index}
+                  color={"secondary"}
+                  lat={hobby?.latitude}
+                  lng={hobby?.longitude}
+                  onClick={() => {
+                    setSelectedData(hobby.id);
+                    console.log("selectedData", hobby.id);
+                  }}
+                />
+              ))}
 
-            {hobbies?.map((hobby, index) => {
-              if (selectedData == hobby.id) {
-                return (
-                  <div
-                    key={index}
-                    lat={hobby?.latitude}
-                    lng={hobby?.longitude}
-                    className="bg-[#ffffff] p-3 text-center relative w-[100px] h-[60px] rounded-md "
-                  >
-                    <Typography
-                      className="absolute left-0 right-0"
-                      style={{ fontSize: "12px" }}
+              {hobbies?.map((hobby, index) => {
+                if (selectedData == hobby.id) {
+                  return (
+                    <div
+                      key={index}
+                      lat={hobby?.latitude}
+                      lng={hobby?.longitude}
+                      className="bg-[#ffffff] p-3 text-center relative w-[100px] h-[60px] rounded-md "
                     >
-                      {hobby.name}
-                    </Typography>
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
-            {searchLatLng?.lat ? (
-              <PushPinIcon
-                style={{ color: "#b00904" }}
-                lat={searchLatLng?.lat}
-                lng={searchLatLng?.lng}
-              />
-            ) : (
-              <MyLocationIcon
-                fontSize="large"
-                color={"primary"}
-                lat={location?.latitude}
-                lng={location?.longitude}
-              />
-            )}
-          </GoogleMapReact>
+                      <Typography
+                        className="absolute left-0 right-0"
+                        style={{ fontSize: "12px" }}
+                      >
+                        {hobby.name}
+                      </Typography>
+                    </div>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+              {searchLatLng?.lat ? (
+                <PushPinIcon
+                  style={{ color: "#b00904" }}
+                  lat={searchLatLng?.lat}
+                  lng={searchLatLng?.lng}
+                />
+              ) : (
+                <MyLocationIcon
+                  fontSize="large"
+                  color={"primary"}
+                  lat={location?.latitude}
+                  lng={location?.longitude}
+                />
+              )}
+            </GoogleMapReact>
+          </div>
+        )}
+        <div className="w-[50%]">
+          {searchLatLng?.lat != null && (
+            <div className="flex gap-1 flex-wrap">
+              {hobbies.map((hobby, index) => (
+                <Card sx={{ maxWidth: 345 }} key={index}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="/static/images/cards/contemplative-reptile.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {hobby?.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              ))}
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
---
+++ src/data/hobbyData.jsx
@@ -0,0 +1,221 @@
+const hobbyData = [
+  {
+    id: 1,
+    name: "NYC Food & Wine Festival",
+    description:
+      "Join us for a week of culinary events featuring world-renowned chefs and wineries from around the globe.",
+    latitude: 40.7421,
+    longitude: -74.0041,
+    image:
+      "https://www.google.com/search?q=cooking+event+new+york+city&rlz=1C1CHBD_enBD1034BD1034&sxsrf=APwXEdfOYU02T3q861Dy0-I2sZmpEMN3TA:1682057918179&source=lnms&tbm=isch&sa=X&ved=2ahUKEwigmNGFqrr-AhW2cmwGHXL5ABsQ_AUoAXoECAEQAw&biw=1536&bih=763&dpr=1.25",
+  },
+
+  {
+    id: 2,
+    name: "Taste of the Upper West Side",
+    description:
+      "Sample food and wine from the Upper West Side's top restaurants and chefs.",
+    latitude: 40.787,
+    longitude: -73.9754,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/taste-of-the-upper-west-side.jpg",
+  },
+
+  {
+    id: 3,
+    name: "New York City Wine & Food Festival",
+    description:
+      "Experience the best food and wine the city has to offer, with proceeds benefiting No Kid Hungry.",
+    latitude: 40.7736,
+    longitude: -73.9772,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/nycwff_header.jpg",
+  },
+
+  {
+    id: 4,
+    name: "The Great GoogaMooga",
+    description:
+      "A food and music festival featuring some of the best restaurants and chefs in the city.",
+    latitude: 40.6689,
+    longitude: -73.9726,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/googamooga_header.jpg",
+  },
+
+  {
+    id: 5,
+    name: "New York Pizza Festival",
+    description:
+      "Try a variety of pizzas from some of the city's best pizzerias.",
+    latitude: 40.7589,
+    longitude: -73.9875,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/pizza-festival-header.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Harlem EatUp!",
+    description:
+      "Experience the diverse culinary scene of Harlem with this multi-day festival.",
+    latitude: 40.8116,
+    longitude: -73.9465,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/harlem-eatup-header.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Taste of Times Square",
+    description:
+      "Sample food and drinks from some of the city's top restaurants in the heart of Times Square.",
+    latitude: 40.7589,
+    longitude: -73.9851,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/taste-of-times-square-header.jpg",
+  },
+
+  {
+    id: 8,
+    name: "Brooklyn Wine Fest",
+    description:
+      "A day of wine tasting from local and international vineyards, along with food from Brooklyn's top restaurants.",
+    latitude: 40.7128,
+    longitude: -73.956,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/brooklyn-wine-fest-header.jpg",
+  },
+
+  {
+    id: 9,
+    name: "The Big Chocolate Show",
+    description:
+      "Indulge in all things chocolate, with tastings, demonstrations, and workshops led by chocolate experts.",
+    latitude: 40.7589,
+    longitude: -73.9851,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/the-big-chocolate-show-header.jpg",
+  },
+
+  {
+    id: 10,
+    name: "NYC Hot Sauce Expo",
+    description:
+      "A spicy food festival featuring hot sauce vendors from around the world.",
+    latitude: 40.7319,
+    longitude: -73.9813,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/hot-sauce-expo-header.jpg",
+  },
+
+  {
+    id: 11,
+    name: "New York Coffee Festival",
+    description:
+      "A celebration of coffee, featuring tastings, workshops, and demonstrations by top baristas and roasters.",
+    latitude: 40.7112,
+    longitude: -74.0104,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/nyc-coffee-festival-header.jpg",
+  },
+
+  {
+    id: 12,
+    name: "New York City BBQ Festival",
+    description:
+      "Sample barbecue from some of the city's top pitmasters, along with live music and other entertainment.",
+    latitude: 40.6769,
+    longitude: -73.9773,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/nyc-bbq-festival-header.jpg",
+  },
+
+  {
+    id: 13,
+    name: "New York Vegandale Food Drink Festival",
+    description:
+      "A festival celebrating all things vegan, with food, drinks, and products from vendors around the city.",
+    latitude: 40.7964,
+    longitude: -73.9691,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/vegandale-festival-header.jpg",
+  },
+
+  {
+    id: 14,
+    name: "New York Oyster Week",
+    description:
+      "Celebrate the city's rich oyster culture with a week of events, including tastings, workshops, and more.",
+    latitude: 40.7417,
+    longitude: -73.9893,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/oyster-week-header.jpg",
+  },
+
+  {
+    id: 15,
+    name: "New York City Dumpling Festival",
+    description:
+      "Sample dumplings from around the world, along with other Asian-inspired foods and drinks.",
+    latitude: 40.7021,
+    longitude: -73.9933,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/dumpling-festival-header.jpg",
+  },
+
+  {
+    id: 16,
+    name: "New York City Beer Week",
+    description:
+      "A week-long celebration of craft beer, featuring tastings, brewery tours, and other events throughout the city.",
+    latitude: 40.7039,
+    longitude: -73.9867,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/beer-week-header.jpg",
+  },
+
+  {
+    id: 17,
+    name: "New York City Wine & Food Festival",
+    description:
+      "One of the city's largest food festivals, featuring tastings, cooking demonstrations, and other events hosted by celebrity chefs.",
+    latitude: 40.7681,
+    longitude: -73.9837,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/nycwff-header.jpg",
+  },
+
+  {
+    id: 18,
+    name: "New York City Pizza Festival",
+    description:
+      "Sample pizza from some of the city's top pizzerias, along with other Italian-inspired foods and drinks.",
+    latitude: 40.7044,
+    longitude: -74.0121,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/pizza-festival-header.jpg",
+  },
+
+  {
+    id: 19,
+    name: "New York City Ice Cream Blizzard",
+    description:
+      "A celebration of ice cream, featuring tastings, workshops, and other events hosted by top ice cream makers from around the city.",
+    latitude: 40.7426,
+    longitude: -73.9879,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/ice-cream-blizzard-header.jpg",
+  },
+
+  {
+    id: 20,
+    name: "New York City Bacon Festival",
+    description:
+      "A festival celebrating all things bacon, with tastings, workshops, and other events hosted by top chefs and food vendors.",
+    latitude: 40.7064,
+    longitude: -74.0094,
+    image:
+      "https://www.nycgo.com/images/uploads/articles/2018/bacon-festival-header.jpg",
+  },
+];
